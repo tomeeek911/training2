963b06d50a3952d14acd2a6ca2e4e3789b715280
tomaszsabiniewicz <tomasz.sabiniewicz@bitgamelabs.com>
2020-07-14 13:37:09 +0200

    update

---
+++ 02-task/helloman.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("hey, this is the first line of the code that I wrote.")
+	second()
+	fmt.Println("And finish all with a Println code")
+}
+func second() {
+	fmt.Println("I can add a 'second' function in the middle.")
+}
